f185f3f2b6af1c5f5e4d7273ebd47837f47f8ac6
Brandon Shannon <bdogshadybanks@gmail.com>
2018-06-16 11:39:25 -0500

    Added unlockable power

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 var clicks = 0;
 var helpers = 0;
+var power =0;
 var currXp = 0;
 var lv = 1;
-var nextXp = (lv +5)*(lv+5);
+var nextXp = 36;
 
 
 function clickUp(num)
 {
-	clicks = clicks + num;
+	clicks = num===1? (power===0?clicks + num : (power*10) + clicks ) : clicks + num;
 	currXp += num;
 	if(currXp >= nextXp)
 	{
@@ -43,25 +44,10 @@ function checkS()
 	}
 }
 
-function xpUpdate()
-{
-	if(currXp >= nextXp)
-	{
-		currXp -= nextXp;
-		lv++;
-		nextXp = (lv +5)*(lv+5);	
-	}
-	var per = currXp / nextXp;
-	per = Math.floor(per*100);
-	var elements = document.querySelectorAll('.graph');
-    for(var i=0; i<elements.length; i++){
-    elements[i].style.width = per + "%";
-}
 
 	
 	
-	
-}
+
 
 function format()
 {
@@ -69,7 +55,7 @@ function format()
 	x = (10*x) + 125
 	document.getElementById("box").style.width = (x + "px");
 }
-
+//pwer for cost+inome per unit= time period
 function buyHelp()
 {
 	var helperCost = Math.floor(10 * Math.pow(1.1,helpers));
@@ -82,9 +68,35 @@ function buyHelp()
 	}
 	var nextCost = Math.floor(10 * Math.pow(1.1,helpers)); 
     document.getElementById("helpCost").innerHTML = nextCost;
-	
+	//10^(1+
+}
+
+function buyPower()
+{
+	if(lv >= 25)
+	{
+		var powerCost = 10000;
+		document.getElementById("power").innerHTML = power;
+		if(clicks >= powerCost)
+		{
+			power++;
+			clicks -= powerCost;
+			document.getElementById("power").innerHTML = power;
+			document.getElementById("clickNum").innerHTML = clicks;
+		}
+		var nextPCost = Math.floor(10000 * Math.pow(1.099,power)); 
+		document.getElementById("powerCost").innerHTML = nextPCost;
+	}
 }
 
 window.setInterval(function(){
 	clickUp(helpers);
+	document.getElementById("clickNum").innerHTML = clicks;
+	document.getElementById("helpNum").innerHTML = helpers;
+	if(lv >= 25)
+	{
+		document.getElementById("powerNum").innerHTML = " Lv: ";
+		document.getElementById("unlockStart").innerHTML = "Click Power";
+		document.getElementById("power").innerHTML = power;
+	}
 }, 1000);
