84c96f8a12b2a33e34d72f47223e1274383525b7
Brandon Shannon <bdogshadybanks@gmail.com>
2018-06-16 02:11:09 -0500

    added a XP bar with levels

--- main.js
+++ main.js
@@ -1,25 +1,66 @@
 var clicks = 0;
 var helpers = 0;
+var currXp = 0;
+var lv = 1;
+var nextXp = (lv +5)*(lv+5);
+
 
 function clickUp(num)
 {
 	clicks = clicks + num;
+	currXp += num;
+	if(currXp >= nextXp)
+	{
+		currXp -= nextXp;
+		lv++;
+		nextXp = (lv +5)*(lv+5);	
+	}
+	var per = currXp / nextXp;
+	per = Math.floor(per*100);
 	document.getElementById("clickNum").innerHTML = clicks;
+	var elements = document.querySelectorAll('.graph');
+    for(var i=0; i<elements.length; i++){
+    elements[i].style.width = per + "%";
+	}
+	document.getElementById("currentXP").innerHTML = currXp;
+	document.getElementById("levelUpXP").innerHTML = nextXp;
+	document.getElementById("level").innerHTML = "xp Lv:" + lv;
 	checkS();
 	format();
+
 	
 }
 
 function checkS()
 {
-	if(clicks === 1)
+	if(helpers === 1)
 	{
-		document.getElementById("needS").innerHTML = "Click!";
+		document.getElementById("needS").innerHTML = "Helper!";
 	}
 	else
 	{
-		document.getElementById("needS").innerHTML = "Clicks!";
+		document.getElementById("needS").innerHTML = "Helpers!";
 	}
+}
+
+function xpUpdate()
+{
+	if(currXp >= nextXp)
+	{
+		currXp -= nextXp;
+		lv++;
+		nextXp = (lv +5)*(lv+5);	
+	}
+	var per = currXp / nextXp;
+	per = Math.floor(per*100);
+	var elements = document.querySelectorAll('.graph');
+    for(var i=0; i<elements.length; i++){
+    elements[i].style.width = per + "%";
+}
+
+	
+	
+	
 }
 
 function format()
@@ -40,12 +81,10 @@ function buyHelp()
 		document.getElementById("clickNum").innerHTML = clicks;
 	}
 	var nextCost = Math.floor(10 * Math.pow(1.1,helpers)); 
-    document.getElementById("helpCost").innerHTML = "Costs " + nextCost + " Clicks";
+    document.getElementById("helpCost").innerHTML = nextCost;
 	
 }
 
 window.setInterval(function(){
-
 	clickUp(helpers);
-
 }, 1000);
